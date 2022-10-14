3e4eb3095cb64a6709556f8730dd55d48701ffca
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2022-10-13 21:02:12 -0400

    Time: 7 ms (83.75%), Space: 7 MB (45.91%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> check;
+        unordered_map<char, char> check1;
+        
+        for(int i=0; i<s.size(); i++){
+                check[s[i]] = t[i];
+        }
+        for(int i=0; i<s.size(); i++){
+                check1[t[i]] = s[i];
+        }
+        
+        for(int i=0; i<s.size(); i++){
+            if(check[s[i]] != t[i] || check1[t[i]] != s[i]) return false;
+        }
+        
+        return true;
+    }
+};
