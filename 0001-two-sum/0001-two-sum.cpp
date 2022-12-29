9ff220e5485938e8d319e9745184d0241cded735
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2022-12-29 05:07:28 -0500

    Time: 17 ms (81.30%), Space: 10.9 MB (30.66%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans(2);
+        unordered_map<int, int> umap;
+        
+        for(int i=0; i<nums.size(); i++){
+            if(umap.find(target - nums[i]) != umap.end()){
+                ans[0] = umap[target - nums[i]];
+                ans[1] = i;
+                break;
+            }
+               
+            umap[nums[i]] = i;
+        }
+        
+        return ans;
+    }
+};
