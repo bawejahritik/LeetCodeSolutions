3c05b7ca4a203a2aa01ccbc9a7e7a8ec89911947
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-01-10 12:15:09 -0500

    Time: 19 ms (74.28%), Space: 19.7 MB (84.71%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -4,23 +4,25 @@ public:
     int trap(vector<int>& height) {
         int n = height.size();
         int waterTrapped = 0;
-        int maxYet = 0;
-        vector<int> maxr(n, 0);
-        maxr[height.size() - 1] = height[height.size() - 1];
-        for(int i=height.size()-2; i>=0; i--){
-            maxr[i] = max(maxr[i+1], height[i]);
+        int maxLeft = 0;
+        int maxRight = 0;
+        int l = 0;
+        int r = n-1;
+        
+        while(l <= r){
+            if(height[l] <= height[r]){
+                if(height[l] >= maxLeft) maxLeft = height[l];
+                else waterTrapped += (maxLeft - height[l]);
+                
+                l++;
+            }else{
+                if(height[r] >= maxRight) maxRight = height[r];
+                else waterTrapped += (maxRight - height[r]);
+                
+                r--;
+            }
         }
         
-        for(int i=0; i<height.size(); i++){
-            //int m1 = getMaxElement(height, 0, i-1);
-            //cout<<m1<<" "<<m2<<" ";
-            int curr = min(maxr[i], maxYet) - height[i];
-            maxYet = max(maxYet, height[i]);
-            if(curr < 0) continue;
-            waterTrapped = waterTrapped + curr;
-            
-            cout<<maxYet<<" ";
-        }
         
         return waterTrapped;
     }
