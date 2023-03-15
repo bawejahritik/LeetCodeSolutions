4be4842ca53def49be8b97ad47c952e77123ddcd
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-03-15 08:35:01 -0400

    Time: 20 ms (86.20%), Space: 19.3 MB (17.07%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int zero = 0;
+        int i=0;
+        
+        while(i<nums.size()){
+            if(nums[i] == 0) {
+                zero++;
+            }
+            else if(zero > 0){
+                nums[i-zero] = nums[i];
+                nums[i] = 0;
+            }
+            i++;
+        }
+        
+    }
+};
