3815933507eb24251e186ae0eadbb16297f52cb6
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-03-18 15:22:25 -0400

    Time: 32 ms (36.73%), Space: 10.7 MB (34.45%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s.size() < 2) return s.size();
-        
-        unordered_map<char,int> count;
+        int len = 0;
         int li = 0;
         int ri = 0;
-        int ans = 0;
+        
+        if(s.size() < 2) return s.size();
+        
+        unordered_map<char, int> counter;
         
         while(ri < s.size()){
-            // cout<<"check "<<count.find(s[ri]) == count.end()<<endl;
-            if(count[s[ri]] == 0){
-                cout<<"insert "<<s[ri]<<endl;
-                count[s[ri]]++;
+            if(counter.find(s[ri]) == counter.end()){
+                counter[s[ri]]++;
                 ri++;
             }else{
-                cout<<"remove "<<s[li]<<endl;
-                count[s[li]]--;
-                if(count[s[li]] == 0) count.erase(s[li]);
+                counter[s[li]]--;
+                if(counter[s[li]] == 0) counter.erase(s[li]);
                 li++;
             }
             
-            ans = max(ri-li, ans);
+            len = max(len, ri-li);
         }
         
-        return ans;
+        return len;
     }
 };
