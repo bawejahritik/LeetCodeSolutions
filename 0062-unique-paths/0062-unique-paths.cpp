4b5ab16823f5536ae4137630fe25292325720f62
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-03-19 01:03:06 -0400

    Time: 3 ms (36.61%), Space: 5.9 MB (89.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int> (n, 0));
+//         vector<vector<int>> dp(m, vector<int> (n, 0));
         
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1] + 1; 
-            }
+//         for(int i=1; i<m; i++){
+//             for(int j=1; j<n; j++){
+//                 dp[i][j] = dp[i-1][j] + dp[i][j-1] + 1; 
+//             }
+//         }
+        
+        long ans = 1;
+        
+        for(int i=m+n-2, j=1; i>=max(m, n); i--, j++){
+            ans = (ans*i)/j;
         }
         
         
-        return dp[m-1][n-1]+1;    
+        return ans;    
     }
 };
