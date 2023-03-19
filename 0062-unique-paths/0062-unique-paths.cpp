297cafb5355a0e6d140e65ef1116687991bc444a
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-03-19 00:56:58 -0400

    Time: 5 ms (13.94%), Space: 6.6 MB (32.94%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        
-        int grid[m][n];
-        
-        for(int i=0; i<m; i++){
-            grid[i][0] = 1;
-        }
-        
-        for(int i=0; i<n; i++){
-            grid[0][i] = 1;
-        }
+        vector<vector<int>> dp(m, vector<int> (n, 0));
         
         for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
-                grid[i][j] = grid[i-1][j] + grid[i][j-1];
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] + 1; 
             }
         }
         
         
-        return grid[m-1][n-1];
+        return dp[m-1][n-1]+1;    
     }
 };
