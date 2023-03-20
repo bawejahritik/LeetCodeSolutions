4986da72ec04fb3255597475d9dc666c58723847
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-03-20 00:16:16 -0400

    Time: 14 ms (52.80%), Space: 7.1 MB (60.99%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int getMaxF(vector<int>& counter){
+        int count = 0;
+        
+        for(int i=0; i<26; i++){
+            count = max(count, counter[i]);
+        }
+        
+        return count;
+    }
+    int characterReplacement(string s, int k) {
+        int ans = 0;
+        
+        vector<int> counter(26, 0);
+        int li = 0;
+        int ri = 0;
+        
+        while(ri < s.size()){
+            counter[s[ri]-'A']++;
+            int maxF = getMaxF(counter);
+            
+            if((ri-li+1) - maxF > k){
+                counter[s[li]-'A']--;
+                li++;
+            }
+            
+            ans = max(ans, ri-li+1);
+            ri++;
+            
+            //cout<<ans<<" ";
+        }
+        
+        return ans;
+    }
+};
