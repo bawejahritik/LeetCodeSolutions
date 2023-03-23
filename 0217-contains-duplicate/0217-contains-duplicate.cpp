eeb5255fe67d08405947a38d805994584f35b9d2
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-03-23 02:43:05 -0400

    Time: 200 ms (19.64%), Space: 73.4 MB (11.78%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, int> counter;
-        
-        for(int i=0; i<nums.size(); i++){
-            if(counter.find(nums[i]) != counter.end()){
-                return true;
-            }
-            
-            counter[nums[i]]++;
-        }
-        
-        return false;
+        return nums.size() > set<int>(nums.begin(), nums.end()).size();
     }
 };
