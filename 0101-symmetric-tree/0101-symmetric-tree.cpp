2597a52f526b5d392783bf7766dfae74a6131d1d
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-03-29 16:50:33 -0400

    Time: 0 ms (100.00%), Space: 16.5 MB (58.81%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -19,8 +19,26 @@ public:
     }
     bool isSymmetric(TreeNode* root) {
         if(root == NULL) return true;
+        queue<TreeNode *> q;
+        q.push(root->left);
+        q.push(root->right);
+        
+        while(!q.empty()){
+            TreeNode *left = q.front();
+            q.pop();
+            TreeNode *right = q.front();
+            q.pop();
+            
+            if(left == NULL && right == NULL) continue;
+            if(left == NULL || right == NULL || left->val != right->val) return false;
+            q.push(left->left);
+            q.push(right->right);
+            q.push(left->right);
+            q.push(right->left);
+        }
         
         
-        return helper(root->left, root->right);
+        //return helper(root->left, root->right);
+        return true;
     }
 };
