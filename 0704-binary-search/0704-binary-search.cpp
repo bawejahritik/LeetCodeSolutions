31008494cbd150017b855c9967576a1192abc678
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-03-31 03:30:00 -0400

    Time: 38 ms (69.68%), Space: 27.6 MB (18.39%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -3,20 +3,16 @@ public:
     int search(vector<int>& nums, int target) {
         int si = 0;
         int ei = nums.size()-1;
-        int mid = si + (ei-si)/2;
+        int mid = (ei-si)/2;
         
         while(si <= ei){
-            if(nums[mid] == target) return mid;
-            else if(nums[mid] > target){
-                ei = mid-1;
-                mid = si + (ei-si)/2;
-            }else{
-                si = mid+1;
-                mid = si + (ei-si)/2;
-            }
+            if(nums[mid] == target)return mid;
+            else if(nums[mid] > target)ei = mid-1;
+            else si = mid+1;
+            
+            mid = si + (ei-si)/2;
         }
         
         return -1;
-        
     }
 };
