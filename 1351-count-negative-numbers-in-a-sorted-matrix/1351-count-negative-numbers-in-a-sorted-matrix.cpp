8cb30d38cf50454050296fde090355c5300a015e
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-04-06 23:55:14 -0400

    Time: 11 ms (96.68%), Space: 10.4 MB (55.64%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -6,9 +6,21 @@ public:
         int count = 0;
         
         for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j] < 0)count++;
+            int si = 0;
+            int ei = n-1;
+            
+            while(si <= ei){
+                int mid = si + (ei-si)/2;
+                
+                if(grid[i][mid] < 0) ei = mid-1;
+                else si = mid+1;
             }
+            
+            count += (n-si);
+            
+            // for(int j=0; j<n; j++){
+            //     if(grid[i][j] < 0)count++;
+            // }
         }
         
         return count;
