d80efb85289e9953fb1aa1a8af4afb27403f8d64
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-04-09 02:25:52 -0400

    Time: 0 ms (100.00%), Space: 10.1 MB (58.54%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,47 +1,24 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size() < nums2.size()){
-            unordered_map<int, int> check1;
-            vector<int> ans;
-            
-            for(int i=0; i<nums1.size(); i++){
-                if(check1[nums1[i]] == 0){
-                    check1[nums1[i]] = 1;
-                }else{
-                    check1[nums1[i]]++;
-                }
-            }
-            
-            for(int i=0; i<nums2.size(); i++){
-                if(check1[nums2[i]] > 0){
-                    check1[nums2[i]]--;
-                    ans.push_back(nums2[i]);
-                }
-            }
-            
-            return ans; 
+        
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        
+        vector<int> ans;
+        int i=0, j=0;
+        
+        while(i < nums1.size() && j < nums2.size()){
+            if(nums1[i] == nums2[j]){
+                ans.push_back(nums1[i]);
+                i += 1;
+                j += 1;
+            }else if(nums1[i] < nums2[j]) i+=1;
+            else j+=1;
         }
         
-        unordered_map<int, int> check2;
-        vector<int> ans;
-            
-        for(int i=0; i<nums2.size(); i++){
-                if(check2[nums2[i]] == 0){
-                    check2[nums2[i]] = 1;
-                }else{
-                    check2[nums2[i]]++;
-                }
-            }
-            
-            for(int i=0; i<nums1.size(); i++){
-                if(check2[nums1[i]] > 0){
-                    check2[nums1[i]]--;
-                    ans.push_back(nums1[i]);
-                }
-            }
-            
-            return ans; 
+        
+        return ans;
         
     }
 };
