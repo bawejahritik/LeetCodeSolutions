4a3de8825407757eb8a604ed66a381dac311c529
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-07-06 09:16:49 +0530

    Time: 68 ms (93.91%), Space: 16.5 MB (16.70%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,22 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        count = 1
+        s = ""
+        
+        for i in range(1, len(chars)):
+            if(chars[i-1] != chars[i]):
+                s += chars[i-1]
+                if count != 1:
+                    s += str(count)
+                count = 1
+            else:
+                count += 1
+                
+        s += chars[-1]
+        if count != 1:
+            s += str(count)
+        
+        for i in range(len(s)):
+            chars[i] = s[i]
+        
+        return len(s)
