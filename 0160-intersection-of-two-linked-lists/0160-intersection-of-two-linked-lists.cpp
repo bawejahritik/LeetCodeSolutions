b22ddb9d1bd68e2f14841ef6d4cfb1c630f2eb33
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-09-10 15:08:59 -0400

    Time: 40 ms (57.94%), Space: 14.5 MB (93.58%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -12,12 +12,12 @@ public:
         ListNode *l1 = headA;
         ListNode *l2 = headB;
         
-        while(l1 != l2){
-            if(l1 == NULL) l1 = headB;
-            else l1 = l1->next;
+        while(l1!=l2){
+            if(l1 == NULL) l1=headB;
+            else l1=l1->next;
             
-            if(l2 == NULL) l2 = headA;
-            else l2 = l2 -> next;
+            if(l2 == NULL) l2=headA;
+            else l2=l2->next;
         }
         
         return l1;
