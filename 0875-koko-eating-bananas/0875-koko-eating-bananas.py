9a24f199e0757c8e22a5de608c9d2f85237e6bd3
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-10-17 03:32:58 -0400

    Time: 441 ms (9.80%), Space: 17.9 MB (61.30%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,19 +1,17 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def can_finish_eating(piles:List[int], h:int, mid:int)->bool:
-            hours_used = 0
-            for p in piles:
-                hours_used += ceil(float(p)/mid)
-            return hours_used<=h
-        left, right = 1, 1000000000
-        boundary_index = -1
+        left, right = 1, max(piles)
+        res = right
         
-        while(left<=right):
-            mid = (left+right)//2
-            if can_finish_eating(piles, h, mid):
-                boundary_index = mid
-                right = mid-1
+        while left<=right:
+            k = (left+right)//2
+            hours = 0
+            for pile in piles:
+                hours += math.ceil(float(pile)/k)
+                
+            if hours<=h:
+                res = k
+                right = k-1
             else:
-                left=mid+1
-        
-        return boundary_index
+                left = k+1
+        return res
