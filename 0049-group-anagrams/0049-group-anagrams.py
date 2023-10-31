b7232e53b1773c885be097737b380c7fbbb457c1
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-10-31 11:18:08 -0700

    Time: 110 ms (25.99%), Space: 22.2 MB (11.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,15 +1,15 @@
-class Solution:
-            
+class Solution:  
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = collections.defaultdict(list)
+        similar = collections.defaultdict(list)
         
         for str in strs:
             temp = [0]*26
-            
             for s in str:
-                temp[ord(s)-97] += 1
+                temp[ord(s)-ord('a')] += 1
             
-            ans[tuple(temp)].append(str)
+            similar[tuple(temp)].append(str)
         
-        return ans.values()
-         
+        
+        
+        
+        return similar.values()
