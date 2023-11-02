a763ccba7fcce03e26a4bb0f2c644fa8747d9928
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-11-02 14:58:00 -0700

    Time: 47 ms (72.32%), Space: 17.1 MB (69.31%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,20 +4,14 @@ class Solution:
         l, r = 0, len(s)-1
         
         while l<r:
-            print(s[l], s[r])
             if s[l].lower() == s[r].lower():
                 l += 1
                 r -= 1
-                print("inside if")
             elif not s[l].isalnum():
                 l += 1
-                print("inside l", s[l].isalpha())
             elif not s[r].isalnum():
                 r -= 1
-                print("inside r")
             else:
                 return False
-        
-        
         return True
     
