5c413892a72c0285eaf56e13add742c8e58b85b2
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-11-02 14:57:23 -0700

    Time: 97 ms (5.02%), Space: 17 MB (69.31%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,17 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        left, right = 0, len(s)-1
         
-        while left<right:
-            if (s[left].isalpha() or s[left].isdigit()) and (s[right].isalpha() or s[right].isdigit()):
-                if s[left].lower() != s[right].lower():
-                    return False
-                else:
-                    left += 1
-                    right -= 1
-            elif not (s[left].isalpha() or s[left].isdigit()):
-                left += 1
+        l, r = 0, len(s)-1
+        
+        while l<r:
+            print(s[l], s[r])
+            if s[l].lower() == s[r].lower():
+                l += 1
+                r -= 1
+                print("inside if")
+            elif not s[l].isalnum():
+                l += 1
+                print("inside l", s[l].isalpha())
+            elif not s[r].isalnum():
+                r -= 1
+                print("inside r")
             else:
-                right -= 1
+                return False
+        
         
         return True
+    
