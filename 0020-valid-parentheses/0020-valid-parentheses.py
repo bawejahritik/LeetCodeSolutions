ea2f18c9335283f37e3515b1b845d202ec7c0639
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-11-17 16:55:33 -0500

    Time: 42 ms (31.84%), Space: 16.3 MB (18.62%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,23 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack =  []
-        
         openB = "({["
-        closeB = ")]}"
+
+        stack = []
         
-        for brack in s:
-            if brack in openB:
-                stack.append(brack)
-            elif stack and (brack in closeB):
-                if (brack == ")" and stack[-1]=="(") or (brack=="]" and stack[-1]=="[") or (brack=="}" and stack[-1]=="{"):
-                    stack.pop()
-                else:
-                    return False
+        for b in s:
+            if b in openB:
+                stack.append(b)
+            elif b==")" and stack and stack[-1]=="(":
+                stack.pop()
+            elif b=="}" and stack and stack[-1]=="{":
+                stack.pop()
+            elif b=="]" and stack and stack[-1]=="[":
+                stack.pop()
             else:
                 return False
         
-        
         return len(stack)==0
                 
-                
-                    
