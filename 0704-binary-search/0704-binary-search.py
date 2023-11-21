0a8357280e55117a4102fb97f70c291105da6e67
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-11-21 07:47:45 -0500

    Time: 207 ms (87.55%), Space: 17.9 MB (7.72%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -4,11 +4,10 @@ class Solution:
         
         while left<=right:
             mid = (left+right)//2
-            
             if nums[mid]==target:
                 return mid
             elif nums[mid]<target:
-                left = mid+1
+                left = mid + 1
             else:
                 right = mid-1
         
