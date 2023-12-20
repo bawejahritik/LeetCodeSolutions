cc250e5f399bdc5b95e1b7387de2238f2dd6886d
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-12-19 22:47:19 -0500

    Time: 52 ms (98.62%), Space: 16.3 MB (55.48%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -1,11 +1,8 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
         prices = sorted(prices)
-        minSpent = float('inf')
         
-        for i in range(len(prices)):
-            for j in range(i+1, len(prices)):
-                if prices[i] + prices[j] <= money:
-                    minSpent = min(minSpent, prices[i] + prices[j])
+        minCost = prices[0] + prices[1]
+        
                     
-        return money - minSpent if minSpent != float('inf') else money
+        return money - minCost if minCost <= money else money
