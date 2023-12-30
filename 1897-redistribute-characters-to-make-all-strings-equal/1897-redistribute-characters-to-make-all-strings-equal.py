8d6aede975f815c7971b11e757aeee711eb30f1d
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2023-12-30 18:17:52 -0500

    Time: 125 ms (5.35%), Space: 17.6 MB (6.29%) - LeetHub

---
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.py
@@ -0,0 +1,17 @@
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        freq = {}
+        
+        for word in words:
+            for char in word:
+                freq[char] = 1 + freq.get(char, 0)
+        
+        
+        for key in freq.keys():
+            if freq[key] % (len(words)) == 0:
+                continue
+            else:
+                return False
+        
+        
+        return True
