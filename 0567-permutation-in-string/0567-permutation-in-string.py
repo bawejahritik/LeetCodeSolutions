1a737f3eae9c440fc670c1bd70b5c2ac3ead8b08
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2024-01-01 22:26:15 -0500

    Time: 54 ms (94.29%), Space: 17.6 MB (10.83%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,30 +1,30 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        
         if len(s1) > len(s2):
             return False
         
+        freqS1 = [0] * 26
         
-        ls1 = [0] * 26
-        ls2 = [0] * 26
+        for s in s1:
+            freqS1[ord(s) - ord('a')] += 1
+
+        window_size = len(s1)
+        window = [0] * 26
         
-        for i in range(len(s1)):
-            ls1[ord(s1[i]) - ord('a')] += 1
-            ls2[ord(s2[i]) - ord('a')] += 1
+        for right in range(window_size):
+            window[ord(s2[right]) - ord('a')] += 1
         
-        if ls1 == ls2:
-            return True
+        if window == freqS1: return True
         
         left = 0
         
-        for right in range(len(s1), len(s2)):
-            ls2[ord(s2[right]) - ord('a')] += 1
-            ls2[ord(s2[left]) - ord('a')] -= 1
-            left += 1
+        for right in range(window_size, len(s2)):
+            window[ord(s2[right]) - ord('a')] += 1
+            window[ord(s2[left]) - ord('a')] -= 1
             
-            if ls1==ls2:
-                return True
-        
-        
+            left += 1
+
+            if window == freqS1: return True
+
         return False
         
