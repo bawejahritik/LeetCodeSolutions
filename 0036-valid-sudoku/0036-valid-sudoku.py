83315959e7ef25b53f88f40858c7bf5af38e8c31
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2024-01-02 20:04:32 -0500

    Time: 128 ms (8.88%), Space: 17.3 MB (12.84%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,37 +1,39 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        def checkRow(row: int) -> bool:
+        ans1 = ans2 = ans3 = True
+        
+        def checkRow(i):
+            temp = []
+            for j in range(0, 9):
+                if board[i][j] != ".":
+                    temp.append(int(board[i][j]))
+            return len(set(temp)) == len(temp)
+        
+        def checkCol(j):
             temp = []
             for i in range(0, 9):
-                if board[row][i] != ".":
-                    temp.append(int(board[row][i]))
+                if board[i][j] != ".":
+                    temp.append(int(board[i][j]))
             return len(set(temp)) == len(temp)
+        
+        def checkBox(row, col):
+            temp = []
             
-        
-        def checkCol(col: int) -> bool:
-            temp = []
-            for i in range(0, 9):
-                if board[i][col] != ".":
-                    temp.append(int(board[i][col]))
-            return len(set(temp)) == len(temp)
-        
-        def checkSquare(row: int, col: int) -> bool:
-            temp = []
             for i in range(0, 3):
                 for j in range(0, 3):
-                    if board[row+i][col+j] != ".":
-                        temp.append(int(board[row+i][col+j]))
+                     if board[row+i][col+j] != ".":
+                            temp.append(int(board[row+i][col+j]))
+            
             return len(set(temp)) == len(temp)
         
-        ans1, ans2, ans3 = True, True, True
-        
-        for row in range(0, 9):
-            ans1 = ans1 and checkRow(row)
-        for col in range(0, 9):
-            ans2 = ans2 and checkCol(col)
-        for row in range(0, 9, 3):
-            for col in range(0, 9, 3):
-                ans3 = ans3 and checkSquare(row, col)
-                    
-        
+        for i in range(0, 9):
+            ans1 = ans1 and checkRow(i)
+            
+        for j in range(0, 9):
+            ans2 = ans2 and checkCol(j)
+            
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                ans3 = ans3 and checkBox(i, j)
+
         return ans1 and ans2 and ans3
