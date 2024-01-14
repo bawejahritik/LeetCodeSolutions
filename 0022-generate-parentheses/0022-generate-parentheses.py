de4fb8dbc91f9a1669e7dd8c48a2bd82afabcf34
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2024-01-14 17:14:49 -0500

    Time: 40 ms (64.25%), Space: 17.6 MB (25.10%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,18 +1,18 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
+        res = []
         
-        stack = []
+        def generator(openCount, closeCount, curr):
+            if openCount == closeCount == n:
+                res.append(curr)
+            
+            if openCount < n:
+                generator(openCount+1, closeCount, curr+"(")
+            
+            if closeCount < openCount:
+                generator(openCount, closeCount+1, curr + ")")
         
-        def generator(o, c, s, n):
-            if o == c == n:
-                stack.append(s)
-                
-            if o<n:
-                generator(o+1, c, s+"(", n)             
-                
-            if c<o:
-                generator(o, c+1, s+")", n)
-                
-        generator(0, 0, "", n)
+        generator(0, 0, "")
         
-        return stack
+        return res
+                
