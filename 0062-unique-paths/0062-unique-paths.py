7162d6e4a5ab2133fbee34b26b61c5cbc7224d3b
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2024-01-22 03:18:48 -0500

    Time: 33 ms (85.66%), Space: 16.7 MB (58.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -8,9 +8,7 @@ class Solution:
             for j in range(n):
                 if i == j == 0:
                     continue
-                elif i == 0:
-                    grid[i][j] = 1
-                elif j == 0:
+                elif i == 0 or j == 0:
                     grid[i][j] = 1
                 else:
                     grid[i][j] = grid[i-1][j] + grid[i][j-1]
