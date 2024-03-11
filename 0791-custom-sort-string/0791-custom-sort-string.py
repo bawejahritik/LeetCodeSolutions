27b7d7c34ec7c0f475448c26b0b957526551169e
bawejahritik <53797247+bawejahritik@users.noreply.github.com>
2024-03-11 00:29:44 -0400

    Time: 26 ms (96.79%), Space: 16.5 MB (91.57%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -1,19 +1,16 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
         freq = {}
-        for i in s:
-            freq[i] = 1 + freq.get(i, 0)
-            
         res = ""
-        
+        for i in s:
+            if i in order:
+                freq[i] = 1 + freq.get(i, 0)
+            else:
+                res += i
+
         for i in order:
             if i in freq:
                 res = res + i*freq[i]
         
-        for key, val in freq.items():
-            if key in res:
-                continue
-            else:
-                res += key*val
         
         return res
